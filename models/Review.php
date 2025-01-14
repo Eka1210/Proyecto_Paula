ebadac48e8f76b475dd99d8d12a3ff8df9f668cd
mariivm <marviquez@estudiantec.cr>
2025-01-14 15:41:01 -0600

    arreglos en modelo review

--- models/Review.php
+++ models/Review.php
@@ -3,14 +3,13 @@ namespace Model;
 
 class Review extends ActiveRecord{
     protected static $tabla = 'review';
-    protected static $columnasDB = ['id','create_time', 'review', 'productID', 'rating', 'userID'];
+    protected static $columnasDB = ['id','create_time', 'review', 'productID', 'rating'];
 
     public $id;
     public $create_time;
     public $review;
     public $productID;
     public $rating;
-    public $userID;
 
     public function __construct($args = []) {
         $this->id = $args['id'] ?? null;
@@ -18,7 +17,6 @@ class Review extends ActiveRecord{
         $this->review = $args['review'] ?? '';
         $this->productID = $args['productID'] ?? null;
         $this->rating = $args['rating'] ?? null; 
-        $this->userID = $args['userID'] ?? null;
     }
 
 }
