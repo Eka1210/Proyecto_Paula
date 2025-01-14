57bb6bdaa294bd187d68b7d2378a468e28d71c3f
frank-the-tankk <frafzhong111@gmail.com>
2025-01-13 20:05:13 -0600

    Model promotionXproduct fix

--- models/ProductXPromotion.php
+++ models/ProductXPromotion.php
@@ -7,13 +7,13 @@ class ProductsxPromotion extends ActiveRecord
     protected static $tabla = 'productXpromotion';
     protected static $columnasDB = ['id', 'productID', 'promotionID'];
 
-    public $cartID;
+    public $id;
     public $productID;
     public $promotionID;
 
     public function __construct($args = [])
     {
-        $this->cartID = $args['cartID'] ?? null;
+        $this->id = $args['id'] ?? null;
         $this->productID = $args['productID'] ?? null;
         $this->promotionID = $args['promotionID'] ?? null;
     }
